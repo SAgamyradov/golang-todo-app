cfaed8e8164ef664203aac1d0b604ae8f29a5197
SAgamyradov <agamyradowserdar777@gmail.com>
2024-05-12 10:55:04 +0500

    add dependency

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	todo "github.com/SAgamyradov/golang-todo-app"
+	"github.com/SAgamyradov/golang-todo-app/pkg/handler"
 )
 
 func main() {
+	handler := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run(": 8000"); err != nil {
+	if err := srv.Run(": 8000", handler.InitRoutes()); err != nil {
 		log.Fatalf(": error occured while running http server: %s", err.Error())
 	}
 }
