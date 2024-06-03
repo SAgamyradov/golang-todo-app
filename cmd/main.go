86f9948a6a5a1b17195d04013e630a0b56cd58cf
SAgamyradov <agamyradowserdar777@gmail.com>
2024-06-03 20:10:53 +0500

    add DB application

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,37 @@ import (
 	"golang-todo-app/pkg/repository"
 	"golang-todo-app/pkg/service"
 	"log"
+	"os"
+
+	_ "github.com/lib/pq"
 
 	"github.com/spf13/viper"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := gotenv.Load(); err != nil {
+		log.Fatalf("error loading env:%s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBname:   viper.GetString("db.dbname"),
+		SSLmode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialaze %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
---
+++ pkg/repository/postgres.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBname   string
+	SSLmode  string
+}
+
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s, port=%s, user=%s, password=%s, dbname=%s, sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DBname, cfg.SSLmode))
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
